c9a41144268847ca5a41f5c7dcf8ae472f304f2e
Domenico Daniel Giovinazzo <domenicodanielgiovinazzo@gmail.com>
2020-11-20 17:08:27 -0500

    finished time_differences project

--- time_differences.rb
+++ time_differences.rb
@@ -31,8 +31,42 @@ def my_min(arr)
 
 end
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p  my_min(list)  # =>  -5
 
 
+def largest_contiguous_subsum_1(list)
+    subs = []
 
+    (0...list.length).each do |i1|
+        (0...list.length).each do |i2|
+            subs << list[i1..i2] if i2 >= i1
+        end
+    end
+    subs.map(&:sum).max
+end
+
+
+def largest_contiguous_subsum(list)
+    current_sum = 0
+    max_sum = 0
+
+    return list.max if list.all?{|num| num < 0} 
+
+    list.each do |num|
+        current_sum + num > 0 ? current_sum += num : current_sum = 0
+        max_sum = current_sum if current_sum > max_sum
+    end
+
+    max_sum
+end
+
+list_1 = [5, 3, -7]
+p largest_contiguous_subsum(list_1) # => 8
+
+list_2 = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list_2) # => 8 (from [7, -6, 7])
+
+list_3 = [-5, -1, -3]
+p largest_contiguous_subsum(list_3) # => -1 (from [-1])
+
+list_4 = [54, -16, -1, 20, 56]
+p largest_contiguous_subsum(list_4) # => -1 (from [-1])
